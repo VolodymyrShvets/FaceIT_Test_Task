94e1ffe01be69e2a9fa7f276ab4df3ddcc85ab68
Volodymyr Shvets <vshvets295@gmail.com>
2023-05-02 19:59:17 +0300

    Added repository layer

---
+++ FaceITTestTask/src/main/java/com/faceit/task/repository/JobsRepository.java
@@ -0,0 +1,18 @@
+package com.faceit.task.repository;
+
+import com.faceit.task.model.Job;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface JobsRepository extends JpaRepository<Job, Long> {
+    @Query("SELECT j.location, COUNT(*) FROM Job j GROUP BY j.location")
+    List<Object[]> countJobsByLocation();
+
+    @Query("SELECT COUNT(*) FROM Job j WHERE LOWER(j.location) = LOWER(:location)")
+    long countJobsByLocationIgnoreCase(@Param("location") String location);
+}
